673a907c64292226c0744c34c7a2d21aa7559c4a
Utopia <utopiamusicnz@gmail.com>
2022-02-18 13:53:59 -0800

    Fixed bugs and added some colour coding on game end

--- script.js
+++ script.js
@@ -2,17 +2,14 @@ let grid = {};
 let cache = {};
 let game = {};
 
-const debug = false; //set to true to skip setup, reveal the game board at game start and get logs when clicking the grid - originally used for debugging the game logic and UI
+const debug = false; //set to true to skip setup, reveal the game board at game start and get logs of gamestate, e.g. when clicking the grid - originally used for debugging the game logic and UI
 
 //INITIALISE
 function startupGame() {
-  if (debug) {
-    console.log("!NOTE: Debug mode is active.");
-  }
+  if (debug) {console.log("!NOTE: Debug mode is active.")};
   cacheUI();
   initialiseGameState();
   initialiseSetup();
-  startGameClock();
 }
 
 function cacheUI() {
@@ -84,22 +81,14 @@ function initialiseSetup() {
     cache.mineNum.innerText = event.target.value;
   });
 
-  checkMineSliderMax(); //set mine slider to match default values
-
+  checkMineSliderMax();
   displaySetupScreen();
   console.log('Displaying setup window');
-  //do UI stuff when resetting
 
-
-  if (debug) {
-    initialiseGame();
-  }
+  if (debug) {initialiseGame()};
 }
 
 function initialiseGame() {
-  // console.log('lets go!');
-  // this.gameWindow = document.getElementById('gameWindow');
-
   //Get player settings input
   grid.rows = cache.rowSel.value;
   grid.cols = cache.colSel.value;
@@ -110,27 +99,20 @@ function initialiseGame() {
   cache.clock.innerText = `Time: 00:00`; //reset clock UI
   cache.resetBtn.style.border = '2px solid darkgrey';
 
-
   console.log("Creating game board");
   setupGrid(); // create the game grid based on the user's inputs during the setup
-
   
   console.log("Placing mines...");
   setupGameMatrices(); //set up grid matrix to operate the gamestate
 
   console.log('Game setup complete');
 
-
   displayGameScreen();
-  updateGameClock();
 
   console.log("Displaying game board")
   render(); //first render of gameboard!
 
-
-
   //Setup over
-
 }
 
 function checkMineSliderMax() {
@@ -142,7 +124,7 @@ function checkMineSliderMax() {
 
 //SETUP
 function setupGrid() {
-  //Dynamically create a CSS grid
+  //Dynamically create a CSS grid to display the game in
   let gridRows = Number(grid.rows);
   let gridCols = Number(grid.cols);
 
@@ -155,7 +137,7 @@ function setupGrid() {
     let gridRowStr = '';
 
     for (let j=0; j<gridCols; j++) {
-      let newID = `${i}-${j}`; //generate an id for the new element based on it's position in the grid
+      let newID = `${i}-${j}`; //generate an id for the new element based on its position in the grid
       generateNewGridElement(newID);
       gridRowStr += `${newID} `;
     }
@@ -223,26 +205,7 @@ function placeMinesInGrid(gameGrid) {
 
 //GAME LOGIC
 function gameController(idx) {
-  if (game.isPlacingFlags && !grid.isRevealed[idx[0]][idx[1]]) {
-    //place flag on grid
-    if (grid.specialIcons[idx[0]][idx[1]] != 'f') {
-      grid.specialIcons[idx[0]][idx[1]] = 'f';
-      game.flagsPlaced += 1;
-    } else {
-      grid.specialIcons[idx[0]][idx[1]] = '';
-      game.flagsPlaced -= 1;
-    }
-    
-  } else if (game.isPlacingQs && !grid.isRevealed[idx[0]][idx[1]]) {
-    //place ? on grid
-    if (grid.specialIcons[idx[0]][idx[1]] != '?') {
-      grid.specialIcons[idx[0]][idx[1]] = '?';
-    } else {
-      grid.specialIcons[idx[0]][idx[1]] = '';
-    }
-
-  } else if ((!grid.isRevealed[idx[0]][idx[1]]) || debug) {
-    //if clicked on unrevealed grid
+  if ((!grid.isRevealed[idx[0]][idx[1]]) || debug) {
     checkGrid(idx); //Check what was revealed on the grid clicked
     checkWin(); //Check if the player has won
   }
@@ -251,7 +214,7 @@ function gameController(idx) {
 
   //then check if special rendering is needed for win/loss
   if (game.end) {
-    clearInterval(game.timer);
+    clearInterval(game.timer); //sometimes the timer doesn't stop???
     if (game.win) {
       //Victory screen
       renderWin();
@@ -263,58 +226,54 @@ function gameController(idx) {
 }
 
 function checkGrid(idx) {
-  //Check the game grid for what is at the given index
-  grid.isRevealed[idx[0]][idx[1]] = true;
-  grid.specialIcons[idx[0]][idx[1]] = '';
+  //Check if flags/?s are to be placed/cleared
+  setFlags(idx);
+  setQs(idx);
 
-  let symb = grid.matrix[idx[0]][idx[1]];
-  if (game.safeSquaresRevealed < 1) {
-    game.timeStartStamp = Date.now();
-  }
-      
-  if (symb === 'X') {
-    if (debug) {
-      console.log(`You clicked on a mine!`)
-    } else if ((game.safeSquaresRevealed < 1) && (grid.mines < 0.9 * grid.area) && (grid.rearrangeCounter < 10)) {
-      //Rearrange grid if struck a mine on the first click and not going overboard with the number of mines in the grid - max ten attempts before just making you lose.
-      //i.e. make it so you don't loose on the first click
-      console.log(`Rearranging grid so you don't loose on the first click: Attempt ${grid.rearrangeCounter+1}`);
-      grid.rearrangeCounter += 1;
-      setupGameMatrices(); //remake gamestate matrix
-      checkGrid(idx); //retry index and check for mines
-    } else {
-      //Struck a mine!!
-      //Game over!
-      loseGame();
+  //Check the game grid for what is at the given index - if not placing a flag/?
+  if (!game.isPlacingFlags && ! game.isPlacingQs) {
+    grid.isRevealed[idx[0]][idx[1]] = true;
+
+    let symb = grid.matrix[idx[0]][idx[1]];
+    if (game.safeSquaresRevealed < 1) {
+      game.timeStartStamp = Date.now();
+      startGameClock();
     }
-  } else if (symb === 0) {
-    game.safeSquaresRevealed += 1;
-    //check adjacent grid for blanks
-    if (debug) {
-      console.log(`You clicked on a blank square`)
-    }
-
-    //If the revealed square is blank, reveal the adjacent ones and then check if they are adjacent to any blanks
-
-    //Get adjacent 
-    let adjGridEls = findAdjacentGrids(Number(idx[0]),Number(idx[1]));
-    let adjGrid = adjGridEls.filter(x=>( //filter out grid indices that are outside the grid 
-      (x[0]>=0) && (x[0] < grid.rows) && (x[1]>=0) && (x[1] < grid.cols)
-    ));
-    //iterate through adjacent grid elements to see if any can be auto-revealed
-    for (adjGridIdx of adjGrid) {
-      if ((grid.matrix[adjGridIdx[0]][adjGridIdx[1]] != 'X') && !grid.isRevealed[adjGridIdx[0]][adjGridIdx[1]]) {
-        grid.isRevealed[adjGridIdx[0]][adjGridIdx[1]] = true;
-        grid.specialIcons[adjGridIdx[0]][adjGridIdx[1]] = '';
-
-        checkGrid(adjGridIdx); //Call this whole function again on the revealed grid tiles and check the tiles adjacent to those
+        
+    if (symb === 'X') { //Mine
+      if (debug) {
+        console.log(`You clicked on a mine!`)
+      } else if ((game.safeSquaresRevealed < 1) && (grid.mines < 0.9 * grid.area) && (grid.rearrangeCounter < 10)) {
+        //Rearrange grid if struck a mine on the first click and not going overboard with the number of mines in the grid - max ten attempts before just making you lose.
+        //i.e. make it so you don't loose on the first click
+        console.log(`Rearranging grid so you don't lose on the first click: Attempt ${grid.rearrangeCounter+1}`);
+        setupGameMatrices(); //remake gamestate matrix
+        grid.rearrangeCounter += 1;
+        checkGrid(idx); //retry index and check for mines
+      } else {
+        //Struck a mine!!
+        loseGame();
       }
-    }
-  } else {
-    //You clicked on a nmber tile and nothing special happens
-    game.safeSquaresRevealed += 1;
-    if (debug) {
-      console.log(`You clicked on a number: ${symb}`)
+    } else if (symb === 0) { //Empty
+      game.safeSquaresRevealed += 1;
+      if (debug) {console.log(`You clicked on a blank square`)};
+
+      //check adjacent grid for blanks
+      let adjGridEls = findAdjacentGrids(Number(idx[0]),Number(idx[1]));
+      let adjGrid = adjGridEls.filter(x=>( 
+        //filter out grid indices that are outside the grid 
+        (x[0]>=0) && (x[0] < grid.rows) && (x[1]>=0) && (x[1] < grid.cols)
+      ));
+      //iterate through adjacent grid elements to see if any can be auto-revealed
+      for (adjGridIdx of adjGrid) {
+        if ((grid.matrix[adjGridIdx[0]][adjGridIdx[1]] != 'X') && !grid.isRevealed[adjGridIdx[0]][adjGridIdx[1]]) {
+
+          checkGrid(adjGridIdx); //Call this whole function again on the revealed grid tiles and check the tiles adjacent to those
+        }
+      }
+    } else { //Number
+      game.safeSquaresRevealed += 1;
+      if (debug) {console.log(`You clicked on a number: ${symb}`)};
     }
   }
 }
@@ -331,6 +290,28 @@ function findAdjacentGrids(r, c) {
   let gTL = [r-1,c-1]; //grid TOP LEFT
 
   return [gT,gTR,gR,gBR,gB,gBL,gL,gTL];
+}
+
+function setFlags(idx){
+  //if square has a flag, clear it. If the square does not have a flag and flags are being placed, place it
+  if (grid.specialIcons[idx[0]][idx[1]] === 'f') {
+    grid.specialIcons[idx[0]][idx[1]] = '';
+    game.flagsPlaced -= 1;
+  } else if (game.isPlacingFlags && (game.flagsPlaced < grid.mines)) {
+    //place flag on grid
+    grid.specialIcons[idx[0]][idx[1]] = 'f';
+    game.flagsPlaced += 1;
+  }
+}
+
+function setQs(idx){
+  //if square has a ?, clear it. If the square does not have a ? and ? are being placed, place it
+  if (grid.specialIcons[idx[0]][idx[1]] === '?') {
+    grid.specialIcons[idx[0]][idx[1]] = '';
+  } else if (game.isPlacingQs) {
+    //place ? on grid
+    grid.specialIcons[idx[0]][idx[1]] = '?';
+  }
 }
 
 function checkWin() {
@@ -359,10 +340,10 @@ function loseGame() {
 }
 
 function revealMines() {
-  //Sets all the mines in the grid to "revealeD" meanign they will show up in the next render
+  //Sets all the unflagged mines in the grid to "revealed" meaning they will show up in the next render
   for (row in grid.matrix) {
     for (col in grid.matrix[row]) {
-      if (grid.matrix[row][col] === 'X') {
+      if ((grid.matrix[row][col] === 'X') && (grid.specialIcons[row][col] != 'f')) {
         grid.isRevealed[row][col] = true;
       }
     }
@@ -375,20 +356,19 @@ function clickTracker(event) {
   if (evEl.id === 'startGame') {
     initialiseGame(); //End setup and start the game
 
-  } else if (((evEl.classList.contains('minesweeperGrid') || evEl.classList.contains('mine')) && !game.end) ) {
+  } else if (((evEl.classList.contains('minesweeperGrid') || evEl.classList.contains('mine')) || evEl.classList.contains('flag')) && !game.end) {
+     //Click in game grid: send the square clicked into the game controller for logic calculations
+    if (debug) {console.log(`You clicked grid no: ${evEl.id}`)};
 
-    if (debug) {
-      console.log(`You clicked grid no: ${evEl.id}`);
-    }
-
-    idx=(evEl.id.split('-')); //turn element clicked ID into a grid reference
-    gameController(idx); //send the grid clicked into the game controller for logic calculations
+    idx=(evEl.id.split('-'));
+    gameController(idx);
     
   } else if (evEl.id == "flagButton") {
-    console.log("Set flags")
+    if (debug) {console.log("Set flags")};
+    
     game.isPlacingFlags = !game.isPlacingFlags; //flip toggle
     game.isPlacingQs = false; //reset ? toggle
-
+    updateToggleBtns()
 
   } else if (evEl.id == "resetButton") {
     //Reset the gamestate
@@ -401,68 +381,101 @@ function clickTracker(event) {
     }
 
   } else if (evEl.id == "questionButton") {
-    console.log("Set ?")
+    if (debug) {console.log("Set ?'s")};
+
     game.isPlacingQs = !game.isPlacingQs; //flip toggle
     game.isPlacingFlags = false; //reset flag toggle
-
+    updateToggleBtns()
   }
 }
 
 //UI
 function render() {
   //display gamestate on screen
-  
-  //Update game header
   renderGameHeader();
-  //update game grid/board/window
   renderGameWindow();
 }
 
 function renderGameWindow() {
+  //Display game state
   for (i in grid.matrix) {
     for (j in grid.matrix[i]) {
       let gridEl = document.getElementById(i+'-'+j);
       if (grid.isRevealed[i][j]) {
-        gridEl.style.backgroundColor = '#e6e6e6'; //reveal square color
-        gridEl.innerText = grid.matrix[i][j]; //set text to value in grid matrix
-
-        gridEl.style.color = getTextColor(i,j); //get colour of number
-        if (grid.matrix[i][j] === 0) {
-          gridEl.style.fontSize = '0px'; //hide text if it is a zero
-        } else if (grid.matrix[i][j] === 'X') {
-          gridEl.innerHTML = `<img src='assets/bomb.png' width=90% class='mine' id='${i+'-'+j}'></img>`; //Put on the bomb image!
-        }
+        //REVEALED SQUARE - BLANK / NUMBER / MINE
+        displayRevealedSquare(gridEl,i,j);
       } else {
-        gridEl.innerText = grid.specialIcons[i][j]; //display flag or ?
-        if (grid.specialIcons[i][j] === 'f') {
-          gridEl.style.color = 'firebrick'; 
-        } else {
+        //UNREVEALED SQUARE - BLANK / FLAG / ?
+        gridEl.innerText = grid.specialIcons[i][j];
+        if (grid.specialIcons[i][j] === 'f') { 
+          //flag on unrevealed square
+          displayFlag(gridEl,i,j);
+        } else if (grid.specialIcons[i][j] === '?') { 
+          //? on unrevealed square
           gridEl.style.color = 'darkslategrey'; 
+          gridEl.style.backgroundColor = '#dedede'; 
+        } else { 
+          //vanilla unrevealed square
+          gridEl.style.color = 'white'; 
+          gridEl.style.backgroundColor = '#d1d1d1';
+          gridEl.innerHTML = '';
         }
-
       }
     }
   }
 }
 
-function renderGameHeader() {
-  //Update game clock
-  if ((game.safeSquaresRevealed > 0) && !game.end) {
-    //no need to update the clock before the player's first move or if the game is over
-    updateGameClock();
-  }
+function displayRevealedSquare(gridEl,i,j) {
+  //Displays and styles a revealed square in the grid
+  gridEl.style.backgroundColor = '#e6e6e6'; //revealed square color
+  gridEl.innerText = grid.matrix[i][j]; //set text to value in grid matrix
+  gridEl.style.color = getTextColor(i,j); //get colour of number
 
-  //visually update toggles
+  if (grid.matrix[i][j] === 0) {
+    //Hide text if empty square
+    gridEl.style.fontSize = '0px';
+  } else if (grid.matrix[i][j] === 'X') {
+    //Orange background for mines!
+    gridEl.style.backgroundColor = '#ffdec7'; 
+    gridEl.innerHTML = `<img src='assets/bomb.png' width=90% class='mine' id='${i+'-'+j}'></img>`; //Put on the bomb image!
+  }
+}
+
+function displayFlag(gridEl,i,j) {
+  //Displays and styles a flag in the grid
+  gridEl.style.color = 'firebrick'; 
+  gridEl.style.backgroundColor = '#dedede'; 
+  gridEl.innerHTML = `<img src='assets/triangle-flag.png' width=65% class='flag' id='${i+'-'+j}'></img>`;
+
+  //At game end, show player if flag placements were correct
+  if (game.end) {
+    if (grid.matrix[i][j] === 'X') {
+      //green background for correct flag placement at game end
+      gridEl.style.backgroundColor = 'rgba(200,255,200,1)'; 
+    } else {
+      //purple background background for bad flag placement at game end
+      gridEl.style.backgroundColor = '#d4c7ff'; 
+      gridEl.innerHTML = `<img src='assets/no-bomb.png' width=90% class='flag' id='${i+'-'+j}'></img>`;
+    }
+  }
+}
+
+function renderGameHeader() { 
+  //Game clock is handled seperately to the main render code & the toggle buttons are handled with each button click.
+
+  //Update mine counter
+  let clearedMines = grid.mines - game.flagsPlaced;
+  cache.mineCount.innerText = `Flags Left: ${clearedMines}`;
+}
+
+function updateToggleBtns() {
+  //colour the clicked toggle and uncolour the others
   let bg = '';
   game.isPlacingFlags ? bg = '#e0e0e0' : bg = 'whitesmoke';
   cache.flagBtn.style.backgroundColor = bg;
 
   game.isPlacingQs ? bg = '#e0e0e0' : bg = 'whitesmoke';
   cache.questionBtn.style.backgroundColor = bg;
-
-  //Update mine counter
-  let clearedMines = grid.mines - game.flagsPlaced;
-  cache.mineCount.innerText = `Flags Left: ${clearedMines}`;
 }
 
 function getTextColor(r,c) {
@@ -493,29 +506,27 @@ function renderLoss() {
   gridEl.innerHTML = `<img src='assets/explosion.png' width=90%></img>`; //Explosion!
   gridEl.style.backgroundColor = 'rgba(255,200,200,1)'; //highlight losing mine
 
-  //Highlight incorrectly marked mines
-
-
-  //
+  //Create lose text
   let newEl = document.createElement('p');
   newEl.id = "gameOverText";
   newEl.innerText = "YOU LOSE";
 
   cache.gameWindow.appendChild(newEl);
 
+  //Red highlight reset button
   cache.resetBtn.style.border = '2px solid red';
-
 }
 
 function renderWin() {
+  //Create win text
   let newEl = document.createElement('p');
   newEl.id = "gameOverText";
   newEl.innerText = "YOU WIN!";
 
   cache.gameWindow.appendChild(newEl);
 
+  //Green highlight reset button
   cache.resetBtn.style.border = '2px solid green';
-
 }
 
 function displaySetupScreen() {
@@ -534,7 +545,7 @@ function startGameClock() {
   //Start the game clock and update the clock UI every second!
   game.timer = setInterval(()=>{
     //every 1s update the game header with the new time
-    renderGameHeader();
+    updateGameClock();
   },1000);
 }
 
@@ -564,4 +575,5 @@ function updateGameClock() {
   cache.clock.innerText = `Time: ${mins}:${secs}`;
 }
 
-startupGame(); //Start the setup for the game and lets go!
+//Start the setup for the game and lets go!
+startupGame();
